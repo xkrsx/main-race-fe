a50ad9a9ea639c7cb33b0a35744092817221ac4f
Jakub Markiewicz <markiewicz267@gmail.com>
2023-03-25 06:49:03 +0100

    Fixing Code Input Validation.

--- src/components/layout/Login/CodeInput.tsx
+++ src/components/layout/Login/CodeInput.tsx
@@ -8,13 +8,27 @@ interface Props {
 
 export const CodeInput = (props: Props) => {
     const [form, setForm] = useState<Props>({
-            code: undefined,
-        })
-    ;
-    const [visible, setVisible] = useState(true);
-    const [courierJobList, setCourierJobList] = useState<CourierViewEntity[] | null>(null);
+        code: undefined,
+    });
     const [loading, setLoading] = useState<boolean>(false);
 
+    // const onUpdateField = (e: FormEvent) => {
+    //     const nextFormState = {
+    //         ...form,
+    //         [key]: value,
+    //     };
+    //     setForm(nextFormState)
+    // };
+
+    const onSubmitForm = (e: FormEvent) => {
+        e.preventDefault();
+        setLoading(true)
+        alert(JSON.stringify(form, null, 2));
+    };
+
+    // const [visible, setVisible] = useState(true);
+    // const [courierJobList, setCourierJobList] = useState<CourierViewEntity[] | null>(null);
+    //
     const updateForm = (key: string, value: any) => {
         setForm(form => ({
             ...form,
@@ -22,49 +36,52 @@ export const CodeInput = (props: Props) => {
         }));
     };
 
-    const refreshView = async () => {
-        setCourierJobList(null);
-        const res = await fetch('http://localhost:3001/login');
-        const data = await res.json();
-        setCourierJobList(data.courierViewList);
-    };
+    // const refreshView = async () => {
+    //     setCourierJobList(null);
+    //     const res = await fetch('http://localhost:3001/login');
+    //     const data = await res.json();
+    //     setCourierJobList(data.courierViewList);
+    // };
 
-    useEffect(() => {
-        refreshView();
-    }, []);
+    // useEffect(() => {
+    //     refreshView();
+    // }, []);
 
-    const codeApproved = () => {
-        setVisible((prev) => !prev);
-    };
+    // const codeApproved = () => {
+    //     setVisible((prev) => !prev);
+    // };
 
-    const codeForm = (e: FormEvent) => {
-        e.preventDefault();
-
-        setLoading(true);
-
-        // if ((form.code_a).length >= 4) {
-        if (form.code === props.code) {
-            console.log('OK')
-            setVisible((prev) => !prev);
-            // codeApproved();
-            //@TODO wywołanie funkcji zamieniającej number input na zielone OK :)
-        }
-        // }
-    };
+    // const codeForm = (e: FormEvent) => {
+    //     e.preventDefault();
+    //
+    //     setLoading(true);
+    //
+    //     // if ((form.code_a).length >= 4) {
+    //     if (form.code === props.code) {
+    //         console.log('OK')
+    //         setVisible((prev) => !prev);
+    //         // codeApproved();
+    //         //@TODO wywołanie funkcji zamieniającej number input na zielone OK :)
+    //     }
+    //     // }
+    // };
 
     return (
-        <form onSubmit={codeForm}>
+        <form onSubmit={onSubmitForm}>
             <label>
                 <td>
-                    {visible && (
-                        <input
-                            type="number"
-                            min={1000}
-                            max={9999}
-                            value={form.code}
-                            onChange={e => updateForm('code', e.target.value)}
-                        />
-                    )}
+                    {/*{visible &&*/}
+                    <input
+                        type="number"
+                        name={"code"}
+                        value={form.code}
+                        // onChange={onUpdateField}
+                        min={1000}
+                        max={9999}
+                        onChange={e => updateForm('code', e.target.value)}
+                    />
+
+                    {/*}*/}
                 </td>
             </label>
         </form>
